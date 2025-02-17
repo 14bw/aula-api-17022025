5e7eb86cee6a3c07dcf65e649f116ae2a0a5b84d
14bw <lennon_ms@outlook.com>
2025-02-17 17:41:57 +0000

    Pending changes exported from your codespace

---
+++ src/main/java/application/controller/IndexController.java
@@ -0,0 +1,14 @@
+package application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+    
+}
