89de29b2656ea4f6bd89ea5b751ccdacc6d27e12
14bw <lennon_ms@outlook.com>
2025-02-18 16:29:10 +0000

    18022025

--- src/main/java/application/controller/IndexController.java
+++ src/main/java/application/controller/IndexController.java
@@ -1,23 +1,13 @@
 package application.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-<<<<<<< HEAD
-=======
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
->>>>>>> 87f641f (17022025)
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
     
-<<<<<<< HEAD
     @RequestMapping("/")
-    public String home() {
-        return "home";
-    }
-=======
-    @GetMapping("/")
     public String home() {
         return "home";
     }
@@ -26,6 +16,4 @@ public class IndexController {
     public String homePost() {
         return "home - POST";
     }
->>>>>>> 87f641f (17022025)
-    
 }
